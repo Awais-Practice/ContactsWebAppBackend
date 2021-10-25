767d87f7bcea7bb83b30d8d787afae83fc32275d
awaismalik <awais.malik.q@gmail.com>
2021-10-25 23:26:53 +0500

    Errors

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const app = express();
 PORT = process.env.PORT || 4000;
+const mysql = require("mysql");
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+// create connection
+const db = mysql.createConnection({
+  host: "31.170.167.61",
+  user: "u768972843_awais",
+  password: "4/OdOc=8#q",
+  database: "u768972843_awais",
+});
+db.connect((err) => {
+  if (err) throw err;
+  console.log("db connected successfully");
+});
+
+app.get("/test", (req, res) => {
+  let sql =
+    "CREATE TABLE contacts(id int AUTO_INCREMENT,firstname VARCHAR(255), lastName VARCHAR(255), email VARCHAR(255), PRIMARY KEY(id))";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  });
+  res.send("create table successfully");
 });
 
 app.listen(PORT, () => console.log(`server is running port number ${PORT}`));
