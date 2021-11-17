18d6528e345ac948ec67af531342ef53d22c275f
awaismalik <awais.malik.q@gmail.com>
2021-11-17 14:33:08 +0500

    In this section of code add contact through postman successfully

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+var cors = require("cors");
 const mysql = require("mysql");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 var db = mysql.createPool({
   connectionLimit: 10,
   host: "31.170.167.61",
@@ -15,7 +20,7 @@ var db = mysql.createPool({
 
 // app.get("/", (req, res) => {
 //   let sql =
-//     "CREATE TABLE contacts(id int AUTO_INCREMENT,firstname VARCHAR(255), lastName VARCHAR(255), email VARCHAR(255), phone VARCHAR(255), PRIMARY KEY(id))";
+//     "CREATE TABLE contacts(id int AUTO_INCREMENT, firstname VARCHAR(255), lastName VARCHAR(255), email VARCHAR(255), phone VARCHAR(255), notes VARCHAR(255), company VARCHAR(255), jobTitle VARCHAR(255), isContactSelect boolean DEFAULT false, PRIMARY KEY(id))";
 //   db.query(sql, (err, ressult) => {
 //     if (err) throw err;
 //     console.log(ressult);
@@ -23,70 +28,102 @@ var db = mysql.createPool({
 //   res.end("table created successFully");
 // });
 
+// app.get("/", (req, res) => {
+//   let sql = "INSERT INTO contacts SET ?";
+//   let contact = {
+//     firstname: "awais",
+//     lastName: "malik",
+//     email: "awais.malik.q@gmail.com",
+//     phone: "0303-0989284",
+//     notes: "this is first note",
+//     company: "freelancer",
+//     jobTitle: "webDevlopment",
+//     isContactSelect: true,
+//   };
+//   db.query(sql, contact, (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+//   res.send("contact add successfully");
+// });
+
 app.get("/", (req, res) => {
   let sql = "INSERT INTO contacts SET ?";
   let contact = {
-    firstname: "awais",
-    lastName: "malik",
-    email: "awais.malik.q@gmail.com",
-    phone: "0303-0989284",
+    firstname: req.body.firstname,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    phone: req.body.phone,
+    notes: req.body.notes,
+    company: req.body.company,
+    jobTitle: req.body.jobTitle,
+    isContactSelect: req.body.isContactSelect,
   };
   db.query(sql, contact, (err, result) => {
     if (err) throw err;
-    console.log(result);
+    res.json(result);
   });
-  res.send("contact add successfully");
 });
 
 // second contact
-app.get("/addtwo", (req, res) => {
-  let sql = "INSERT INTO contacts SET ?";
-  let contact = {
-    firstname: "mohsin",
-    lastName: "Raza",
-    email: "malikMohsin.q@gmail.com",
-    phone: "03429996029",
-  };
-  db.query(sql, contact, (err, result) => {
-    if (err) throw err;
-    console.log(result);
-  });
-  res.send("contact add successfully");
-});
+// app.get("/addtwo", (req, res) => {
+//   let sql = "INSERT INTO contacts SET ?";
+//   let contact = {
+//     firstname: "mohsin",
+//     lastName: "Raza",
+//     email: "malikMohsin.q@gmail.com",
+//     phone: "03429996029",
+//   };
+//   db.query(sql, contact, (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+//   res.send("contact add successfully");
+// });
 // get all  contacts information
 
 app.get("/allcontacts", (req, res) => {
   let sql = "SELECT * FROM contacts";
   db.query(sql, (err, results) => {
-    console.log(results);
+    res.json(results);
   });
-  res.send("all postes fatched in console log");
 });
 
 // select single contact from db
 
-app.get("/selectedcontact/:id", (req, res) => {
-  let sql = `SELECT * FROM contacts WHERE id = ${req.params.id}`;
-  db.query(sql, (err, results) => {
-    if (err) throw err;
-    console.log(results);
-  });
-  res.send("contact selected through id single");
-});
+// app.get("/selectedcontact/:id", (req, res) => {
+//   let sql = `SELECT * FROM contacts WHERE id = ${req.params.id}`;
+//   db.query(sql, (err, results) => {
+//     if (err) throw err;
+//     res.json(results);
+//   });
+//   res.send("contact selected through id single");
+// });
 
 // update single contact
 
-app.get("/awais/:id", (req, res) => {
-  let userFname = "awaismalik";
-  let sql = `UPDATE contacts SET firstname = '${userFname}' WHERE id = ${req.params.id}`;
+// app.get("/awais/:id", (req, res) => {
+//   let userFname = "awaismalik";
+//   let sql = `UPDATE contacts SET firstname = '${userFname}' WHERE id = ${req.params.id}`;
 
-  db.query(sql, (err, result) => {
-    if (err) throw err;
-    console.log(result);
-  });
-  res.send("contact first name update successfully");
-});
+//   db.query(sql, (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+//   res.send("contact first name update successfully");
+// });
 
 app.listen(PORT, () => {
   console.log(`server is running port number ${PORT}`);
 });
+
+//  id: 1,
+//   firstName: 'muhammad awais',
+//   email: 'awais.malik.q@gmail.com',
+//   phone: '0303-0989284',
+//   notes: 'this is notes data ',
+//   company: 'testing',
+//   jobTitle: 'free',
+//   img: 'assets/images/user.jpeg',
+//   lastName: 'malik',
+//   isContactSelect: false,
